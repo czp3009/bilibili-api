2f1a765f5aea9637531726d656fa086555aeb0cf
czp3009 <czp3009@gmail.com>
2019-03-04 00:02:56 +0800

    修正从流中读取 byte 时读到负数的问题

--- src/main/kotlin/com/hiczp/bilibili/api/StreamExtension.kt
+++ src/main/kotlin/com/hiczp/bilibili/api/StreamExtension.kt
@@ -11,18 +11,19 @@ fun InputStream.readFully(length: Int) = IOUtils.readFully(this, length)!!
 /**
  * 以大端模式从流中读取一个 int
  */
+@UseExperimental(ExperimentalUnsignedTypes::class)
 fun InputStream.readInt(): Int {
     val byteArray = readFully(4)
-    return (byteArray[0].toInt() shl 24) or
-            (byteArray[1].toInt() shl 16) or
-            (byteArray[2].toInt() shl 8) or
-            (byteArray[3].toInt())
+    return (byteArray[0].toUByte().toInt() shl 24) or
+            (byteArray[1].toUByte().toInt() shl 16) or
+            (byteArray[2].toUByte().toInt() shl 8) or
+            (byteArray[3].toUByte().toInt())
 }
 
 /**
  * 以大端模式从流中读取一个 unsigned int
  */
-@Suppress("EXPERIMENTAL_API_USAGE")
+@UseExperimental(ExperimentalUnsignedTypes::class)
 fun InputStream.readUInt() = readInt().toUInt()
 
 fun InputStream.boundedReader(maxCharsFromTargetReader: Int, charset: Charset = Charsets.UTF_8) =
