9e7f6a4830ce45975a81bd1d7d71b0ee5756878f
yiweigang <522240446@qq.com>
2020-01-24 14:13:47 +0800

    允许外部代码获取响应体

--- src/main/kotlin/com/hiczp/bilibili/api/retrofit/exception/BilibiliApiException.kt
+++ src/main/kotlin/com/hiczp/bilibili/api/retrofit/exception/BilibiliApiException.kt
@@ -7,5 +7,5 @@ import java.io.IOException
  * 当服务器返回的 code 不等于 0 时抛出
  */
 class BilibiliApiException(
-        commonResponse: CommonResponse
+        val commonResponse: CommonResponse
 ) : IOException(commonResponse.message?.takeIf { it.isNotEmpty() } ?: commonResponse.msg)
